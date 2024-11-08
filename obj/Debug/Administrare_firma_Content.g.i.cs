683dc0e509592be276c41e7a7fbbbb19d12137ef
smoggydesire <vborcila10@gmail.com>
2024-11-08 17:16:14 +0200

    Save remaining changes

--- obj/Debug/Administrare_firma_Content.g.i.cs
+++ obj/Debug/Administrare_firma_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/finances.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute(".gitignore")]
 
 
